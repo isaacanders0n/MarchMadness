c10ab99a238506daa37f6b5ad942978650f7d34e
Isaac Anderson <isaac.anderson@radioninsurance.com>
2024-03-28 18:43:46 -0500

    Dagster example

--- DAG/DAGCreator.py
+++
@@ -1,6 +0,0 @@
-import dagster as dg 
-import pandas as pd
-import sys
-
-sys.path.append('../ncaa_mod')
-from ncaa_mod import cleaning as c
--- projectDag/projectDag/assets.py
+++ projectDag/projectDag/assets.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import sys
+import os
+from dagster import asset
+
+@asset 
+def hello():
+    return pd.DataFrame({'hello': ['world']})
+
+
+@asset
+def goodbye():
 
