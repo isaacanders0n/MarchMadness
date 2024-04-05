de579a11d1d3b2c187e04b48d04f04a1db1c0b27
Isaac Anderson <isaac.anderson@radioninsurance.com>
2024-04-05 13:21:30 -0500

    fixes

--- projectDag/projectDag/assets/EDA/__init__.py
+++ projectDag/projectDag/assets/EDA/__init__.py
@@ -10,10 +10,29 @@ import base64
 
 
 @asset(ins = {'ncaa_cleaned': AssetIn('ncaa_cleaned')})
-def corr(ncaa_cleaned: pd.DataFrame):
+def vis_dataset(ncaa_cleaned: pd.DataFrame):
+    '''Preview of our data'''
+    return ncaa_cleaned
+
+@asset()
+def corr(vis_dataset: pd.DataFrame):
     '''Correlation Matrix of our data'''
     #plot scatter matrix
-    fig = px.scatter_matrix(ncaa_cleaned, dimensions = ['ADJOE', 'ADJDE', 'BARTHAG', 'EFG_O', 'EFG_D', 'TOR'])
+    fig = px.scatter_matrix(vis_dataset, dimensions = ['ADJOE', 'ADJDE', 'BARTHAG', 'EFG_O', 'EFG_D', 'TOR'])
+    
+    #save in memory
+    bytes_img = pio.to_image(fig, format="png", width=800, height=600, scale=2.0)
+    image_data = base64.b64encode(bytes_img)
+    #convert to markdown for preview
+    md_content = f"![img](data:image/png;base64,{image_data.decode()})"
+
+    return MaterializeResult(metadata={"plot": MetadataValue.md(md_content)})
+
+@asset()
+def boxplot(vis_dataset: pd.DataFrame):
+    '''Boxplot of our data'''
+    #plot boxplot
+    fig = px.box(vis_dataset, x = 'POSTSEASON', y = 'ADJOE')
     
     #save in memory
     bytes_img = pio.to_image(fig, format="png", width=800, height=600, scale=2.0)
